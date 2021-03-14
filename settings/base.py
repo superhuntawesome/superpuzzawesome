2d6c71706fe02eb914465ca3afa1866ff077d317
styu <styu314@gmail.com>
2021-03-13 18:43:08 -0600

    Add deploy workflow and fix allowed hosts (#7)

--- settings/base.py
+++ settings/base.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import datetime
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "FIXME_SECRET_KEY_GOES_HERE")
+SECRET_KEY = os.environ.get("SECRET_KEY", os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,9 +94,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -157,7 +167,11 @@ LOGGING = {
         },
     },
     "loggers": {
-        "django": {"handlers": ["django"], "level": "DEBUG", "propagate": True,},
+        "django": {
+            "handlers": ["django"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
         "puzzles.puzzle": {
             "handlers": ["puzzle"],
             "level": "DEBUG",
--- settings/staging.py
+++ settings/staging.py
@@ -2,4 +2,6 @@ from settings.base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['.www.example.com'] # FIXME: where are you hosting puzzlord?
+ALLOWED_HOSTS = [
+    "hunt-staging.superteamaweso.me"
+]  # FIXME: where are you hosting puzzlord?
