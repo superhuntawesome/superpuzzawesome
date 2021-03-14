81f06e90cf9ad5315ccf5e435e207a60c4995447
Stephanie Yu <stephaniey@palantir.com>
2021-03-13 18:50:22 -0600

    Actually fix allowed hosts

--- settings/staging.py
+++ settings/staging.py
@@ -3,5 +3,5 @@ from settings.base import *
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "hunt-staging.superteamaweso.me"
+    ".hunt-staging.superteamaweso.me"
 ]  # FIXME: where are you hosting puzzlord?
