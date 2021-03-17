e41e54b55897f3056809f805749d1867602331fa
styu <styu314@gmail.com>
2021-03-16 23:38:24 -0400

    Update static files path for production (#16)

--- settings/base.py
+++ settings/base.py
@@ -125,7 +125,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "static"))
-
+STATICFILES_DIRS = [
+    os.path.normpath(os.path.join(BASE_DIR, "puzzle_editing", "static"))
+]
 
 LOGIN_URL = "/login"
 LOGIN_REDIRECT_URL = "/"
